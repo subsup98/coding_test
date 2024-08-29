b9b49484e9d014290bfbac9159fda07a380f8c04
limyongsub <dydtjqdla98@kyonggi.ac.kr>
2024-08-29 18:36:07 +0900

    day1

---
+++ dat1.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+def solution(k, dungeons):
+    
+    all_per =permutations(dungeons)
+    max_du=0
+    z= list(all_per)
+    for per in z:
+        hp=k
+        count=0
+        for min_hp,loss in per:
+            if hp< min_hp:
+                break
+            else:
+                count+=1
+                hp -=loss
+            max_du=max(max_du,count)
+    return max_du
