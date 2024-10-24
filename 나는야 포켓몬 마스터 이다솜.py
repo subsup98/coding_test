fbc7874cc4e27a86461afc50a3768ef3ec42c680
limyongsub <dydtjqdla98@kyonggi.ac.kr>
2024-10-24 18:12:04 +0900

    포켓몬 마스터 에러 수정

--- 나는야 포켓몬 마스터 이다솜.py
+++ 나는야 포켓몬 마스터 이다솜.py
@@ -1,20 +1,23 @@
 import sys
-n,m = map(int,sys.stdin.readline().split())
+n, m = map(int, sys.stdin.readline().split())
+querys = []
+pokemons_index = [0]  
+pokemons_name = {} 
 
-pokemons=[0]
-querys=[]
-for i in range(1,n+1):
+
+for i in range(1, n + 1):
     pokemon = sys.stdin.readline().rstrip()
-    pokemons.append(pokemon)
+    pokemons_index.append(pokemon)
+    pokemons_name[pokemon] = i  
+
 for j in range(m):
-    query=sys.stdin.readline().rstrip()
+    query = sys.stdin.readline().rstrip()
     querys.append(query)
 
 for query in querys:
-    if query.isdigit():
-        print(pokemons[int(query)])
-
-    else:
-        print(pokemons.index(query))
     
-
+    if query.isdigit():
+        print(pokemons_index[int(query)])
+    
+    else:
+        print(pokemons_name[query])
