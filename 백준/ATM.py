0e222bf059bb1edf6a55b12539bbbc3f750fe4af
limyongsub <dydtjqdla98@kyonggi.ac.kr>
2024-11-14 22:16:06 +0900

    ATM

--- 백준/ATM.py
+++ 백준/ATM.py
@@ -2,7 +2,7 @@ import sys
 n =int(sys.stdin.readline().strip())
 time = list(map(int,sys.stdin.readline().strip().split()))
 time.sort()
-print(time)
+
 ans=0
 for i in range(len(time)-1):
     ans +=time[i]
